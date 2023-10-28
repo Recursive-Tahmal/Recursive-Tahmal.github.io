be216c709755a52e7d20ebde359a3728b131d2ba
Tahmal <tahmal565@gmail.com>
2023-10-29 00:14:15 +0300

    Added certificates to about page

--- js/scripts.js
+++ js/scripts.js
@@ -18,19 +18,19 @@
 
 
 // Get all elements with class 'project-img'
-var projectImages = document.querySelectorAll('.project-img');
+var projectImages = document.querySelectorAll('.img');
 
 // Add event listeners to each image
 projectImages.forEach(function (img) {
     // Click event: Toggle width on click
     img.addEventListener('click', function () {
         // Check current width and toggle between 10% and 100%
-        if (img.style.width === '75%') {
+        if (img.style.width === '50%') {
             img.style.width = '10%';
             // Change the text to "Click to expand"
             img.nextElementSibling.innerText = 'Click to expand';
         } else {
-            img.style.width = '75%';
+            img.style.width = '50%';
             // Change the text to "Click to shrink"
             img.nextElementSibling.innerText = 'Click to shrink';
         }
