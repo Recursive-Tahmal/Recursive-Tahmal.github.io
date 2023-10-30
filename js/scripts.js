61f5d74a6e762305c3c92bd7c71595f468002185
Tahmal <tahmal565@gmail.com>
2023-10-30 16:30:03 +0300

    Fixed responsive design issues

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,10 @@
-/**
- * Used for expanding and shrinking images
- */
-// Get all elements with class 'project-img'
-var projectImages = document.querySelectorAll('.img');
+document.addEventListener('DOMContentLoaded', function () {
+    var projectImages = document.querySelectorAll('.img');
 
-// Add event listeners to each image
-projectImages.forEach(function (img) {
-    // Click event: Toggle width on click
-    img.addEventListener('click', function () {
-        // Check current width and toggle between 10% and 100%
-        if (img.style.width === '50%') {
-            img.style.width = '10%';
-            // Change the text to "Click to expand"
-            img.nextElementSibling.innerText = 'Click to expand';
-        } else {
-            img.style.width = '50%';
-            // Change the text to "Click to shrink"
-            img.nextElementSibling.innerText = 'Click to shrink';
-        }
+    projectImages.forEach(function (img) {
+        img.addEventListener('click', function () {
+            img.classList.toggle('clicked');
+            img.nextElementSibling.innerText = img.classList.contains('clicked') ? 'Click to shrink' : 'Click to expand';
+        });
     });
 });
